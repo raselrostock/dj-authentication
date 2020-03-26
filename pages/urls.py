8f805ecd64438a3a33cdaba003c8ff41240dd8bc
raselrostock <raselrostock@gmail.com>
2020-03-26 15:23:18 +0600

    added signup functionality

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from pages.views import home_view
+from pages.views import home_view, signup_view
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('', home_view, name='home')
+    path('', home_view, name='home'),
+    path('signup/', signup_view, name='signup'),
 ]
--- pages/views/home.py
+++ pages/views/home.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 def home_view(request):
+    user = User.objects.all().count()
     context =  {
-        'title': 'Home Page'
+        'title': 'Home Page',
+        'users': user
     }
     return render(request, 'pages/home.html', context)
